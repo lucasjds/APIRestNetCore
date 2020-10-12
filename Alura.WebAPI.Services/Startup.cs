85b6384b001fd7a629a02b2ad69bdaa31dace91d
lucas <lucassouza690@gmail.com>
2020-10-12 18:57:08 -0300

    Separando a autenticação da API

---
+++ Alura.WebAPI.Services/Startup.cs
@@ -0,0 +1,48 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Alura.ListaLeitura.Seguranca;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+
+namespace Alura.WebAPI.Services
+{
+  public class Startup
+  {
+    // This method gets called by the runtime. Use this method to add services to the container.
+    // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
+    public void ConfigureServices(IServiceCollection services)
+    {
+      services.AddDbContext<AuthDbContext>(options =>
+      {
+        options.UseSqlServer(Configuration.GetConnectionString("AuthDB"));
+      });
+
+      services.AddIdentity<Usuario, IdentityRole>(options =>
+      {
+        options.Password.RequiredLength = 3;
+        options.Password.RequireNonAlphanumeric = false;
+        options.Password.RequireUppercase = false;
+        options.Password.RequireLowercase = false;
+      }).AddEntityFrameworkStores<AuthDbContext>();
+
+      services.AddMvc();
+    }
+
+    // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+    public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+    {
+      if (env.IsDevelopment())
+      {
+        app.UseDeveloperExceptionPage();
+      }
+
+      app.UseMvc();
+    }
+  }
+}
